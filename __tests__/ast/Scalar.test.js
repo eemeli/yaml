3b5e01c8c1b4f5d8f4455c3fd8e580aeb43a031a
Eemeli Aro <eemeli@gmail.com>
2018-01-14 05:12:15 +0200

    Generalise test wrappers, split them to their own file

--- __tests__/ast/Scalar.test.js
+++ __tests__/ast/Scalar.test.js
@@ -1,63 +1,29 @@
-import Document from '../../src/ast/Document'
-import Node from '../../src/ast/Node'
-
-const testScalarParse = ({ pre, post, str, comment, expected, inFlow, startIdx, test: customTest }) => {
-  let body = str
-  if (comment) {
-    const lines = body.split('\n')
-    lines[0] += ` #${comment}`
-    body = lines.join('\n')
-  }
-  const doc = new Document(pre + body + post)
-  const indent = Node.endOfIndent(doc.src, 0)
-  const scalar = doc.parseNode(startIdx || pre.length, indent, inFlow || false)
-  expect(scalar.rawValue).toBe(expected || str)
-  const expectedEnd = Node.endOfWhiteSpace(doc.src, pre.length + body.length)
-  expect(scalar.range.end).toBe(expectedEnd)
-  if (comment) expect(scalar.comment).toBe(comment)
-  if (customTest) customTest(scalar)
-  delete scalar.doc
-  expect(scalar).toMatchSnapshot()
-}
-
-const commonTests = {
-  'bare': { pre: '', post: '' },
-  'newline before & after': { pre: '\n', post: '\n' },
-  'complex mapping key': { pre: '? ', post: ' : ' },
-  'seq value': { pre: '- ', post: '\n- ' },
-  'indented block': { pre: '    - ', post: '\n  x' },
-  'flow seq value': { pre: '[ ', post: ' ]', inFlow: true },
-  'with comment': { pre: '\n  ', comment: 'comment # here!', post: '\n' },
-  'with props': { pre: '- !tag! &anchor ', post: '\n- ', startIdx: 2, test: (scalar) => {
-    expect(scalar.anchor).toBe('anchor')
-    expect(scalar.tag).toBe('tag!')
-  } }
-}
+import { commonTests, testParse } from './common'
 
 describe('parse "quoted"', () => {
   for (const name in commonTests) {
     const props = Object.assign({ str: '"value"' }, commonTests[name])
-    test(name, () => testScalarParse(props))
+    test(name, () => testParse(props))
   }
-  test('without spaces', () => testScalarParse({ pre: '{', str: '"value"', post: ',' }))
-  test('multi-line', () => testScalarParse({ pre: '\n', str: '"value\nwith\nmore lines"', post: '\n' }))
-  test('escaped', () => testScalarParse({ pre: '\n', str: '" #value\\\\\nwith \\"more\\" lines\\""', post: '\n' }))
+  test('without spaces', () => testParse({ pre: '{', str: '"value"', post: ',' }))
+  test('multi-line', () => testParse({ pre: '\n', str: '"value\nwith\nmore lines"', post: '\n' }))
+  test('escaped', () => testParse({ pre: '\n', str: '" #value\\\\\nwith \\"more\\" lines\\""', post: '\n' }))
 })
 
 describe("parse 'quoted'", () => {
   for (const name in commonTests) {
     const props = Object.assign({ str: "'value'" }, commonTests[name])
-    test(name, () => testScalarParse(props))
+    test(name, () => testParse(props))
   }
-  test('without spaces', () => testScalarParse({ pre: '{', str: "'value'", post: ',' }))
-  test('multi-line', () => testScalarParse({ pre: '\n', str: "'value\nwith\nmore lines'", post: '\n' }))
-  test('escaped', () => testScalarParse({ pre: '\n', str: "' #value\nwith ''more'' lines'''", post: '\n' }))
+  test('without spaces', () => testParse({ pre: '{', str: "'value'", post: ',' }))
+  test('multi-line', () => testParse({ pre: '\n', str: "'value\nwith\nmore lines'", post: '\n' }))
+  test('escaped', () => testParse({ pre: '\n', str: "' #value\nwith ''more'' lines'''", post: '\n' }))
 })
 
 describe("parse *alias", () => {
   for (const name in commonTests) {
     const props = Object.assign({ str: '*alias', expected: 'alias' }, commonTests[name])
-    test(name, () => testScalarParse(props))
+    test(name, () => testParse(props))
   }
 })
 
@@ -69,19 +35,19 @@ describe("parse >block", () => {
       props.pre = `  ${props.pre}`
       props.post = props.post.replace(/^\s?/, '\n')
     }
-    test(name, () => testScalarParse(props))
+    test(name, () => testParse(props))
   }
-  test('literal with header', () => testScalarParse({ pre: '\n- ', str: `|+2\n${block}`, expected: block, post: '\n' }))
+  test('literal with header', () => testParse({ pre: '\n- ', str: `|+2\n${block}`, expected: block, post: '\n' }))
 })
 
 describe("parse single-line plain", () => {
   for (const name in commonTests) {
     const props = Object.assign({ str: 'plain value' }, commonTests[name])
-    test(name, () => testScalarParse(props))
+    test(name, () => testParse(props))
   }
-  test('escaped', () => testScalarParse({ pre: '', str: '-#:] ,', post: ': ' }))
-  test('escaped in flow', () => testScalarParse({ pre: '', str: '-#:', post: '] ,: ', inFlow: true }))
-  test('empty', () => testScalarParse({ pre: '- ', str: '', post: ' : ' }))
+  test('escaped', () => testParse({ pre: '', str: '-#:] ,', post: ': ' }))
+  test('escaped in flow', () => testParse({ pre: '', str: '-#:', post: '] ,: ', inFlow: true }))
+  test('empty', () => testParse({ pre: '- ', str: '', post: ' : ' }))
 })
 
 describe("parse multi-line plain", () => {
@@ -96,21 +62,21 @@ describe("parse multi-line plain", () => {
       props.pre = `  ${props.pre}`
       props.post = props.post.replace(/^\s?/, '\n')
     }
-    test(name, () => testScalarParse(props))
+    test(name, () => testParse(props))
   }
 })
 
 describe('parse %directives', () => {
   const tag = 'TAG !yaml! tag:yaml.org,2002:'
-  test('TAG', () => testScalarParse({ pre: '', post: '\n', str: `%${tag}`, expected: tag }))
-  test('YAML with comment', () => testScalarParse({ pre: '', post: '\n', str: '%YAML 1.2', expected: 'YAML 1.2', comment: 'comment' }))
+  test('TAG', () => testParse({ pre: '', post: '\n', str: `%${tag}`, expected: tag }))
+  test('YAML with comment', () => testParse({ pre: '', post: '\n', str: '%YAML 1.2', expected: 'YAML 1.2', comment: 'comment' }))
 })
 
 describe('parse #comments', () => {
   const comment = 'comment # here!'
-  test('bare', () => testScalarParse({ pre: '', str: '', comment, post: '' }))
-  test('newline before & after', () => testScalarParse({ pre: '\n', str: '', comment, post: '\n' }))
-  test('complex mapping key', () => testScalarParse({ pre: '? ', str: '', comment, post: '\n: ' }))
-  test('seq value', () => testScalarParse({ pre: '- ', str: '', comment, post: '\n- ' }))
-  test('indented block', () => testScalarParse({ pre: '    - ', str: '', comment, post: '\n  x' }))
+  test('bare', () => testParse({ pre: '', str: '', comment, post: '' }))
+  test('newline before & after', () => testParse({ pre: '\n', str: '', comment, post: '\n' }))
+  test('complex mapping key', () => testParse({ pre: '? ', str: '', comment, post: '\n: ' }))
+  test('seq value', () => testParse({ pre: '- ', str: '', comment, post: '\n- ' }))
+  test('indented block', () => testParse({ pre: '    - ', str: '', comment, post: '\n  x' }))
 })
---
+++ __tests__/ast/common.js
@@ -0,0 +1,36 @@
+import Document from '../../src/ast/Document'
+import Node from '../../src/ast/Node'
+
+export const testParse = ({ pre, post, str, comment, expected, inFlow, startIdx, test: customTest }) => {
+  let body = str
+  if (comment) {
+    const lines = body.split('\n')
+    lines[0] += ` #${comment}`
+    body = lines.join('\n')
+  }
+  const doc = new Document(pre + body + post)
+  const indent = Node.endOfIndent(doc.src, 0)
+  const node = doc.parseNode(startIdx || pre.length, indent, inFlow || false)
+  expect(node.rawValue).toBe(expected || str)
+  const expectedEnd = Node.endOfWhiteSpace(doc.src, pre.length + body.length)
+  expect(node.range.end).toBe(expectedEnd)
+  if (comment) expect(node.comment).toBe(comment)
+  if (customTest) customTest(node)
+  delete node.doc
+  expect(node).toMatchSnapshot()
+}
+
+export const commonTests = {
+  'bare': { pre: '', post: '' },
+  'newline before & after': { pre: '\n', post: '\n' },
+  'complex mapping key': { pre: '? ', post: ' : ' },
+  'seq value': { pre: '- ', post: '\n- ' },
+  'indented block': { pre: '    - ', post: '\n  x' },
+  'flow seq value': { pre: '[ ', post: ' ]', inFlow: true },
+  'with comment': { pre: '\n  ', comment: 'comment # here!', post: '\n' },
+  'with props': { pre: '- !tag! &anchor ', post: '\n- ', startIdx: 2, test: (node) => {
+    expect(node.anchor).toBe('anchor')
+    expect(node.tag).toBe('tag!')
+  } }
+}
+
