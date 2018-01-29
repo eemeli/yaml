870fdad4c9b1b8d25aec310ab4d5646d3ff2627c
Eemeli Aro <eemeli@gmail.com>
2018-01-29 17:15:23 +0200

    Fix error constructors

--- src/errors.js
+++ src/errors.js
@@ -1,20 +1,23 @@
 export class YAMLReferenceError extends ReferenceError {
   constructor (source, message) {
-    super(message)
+    super()
+    this.message = message
     this.source = source
   }
 }
 
 export class YAMLSyntaxError extends SyntaxError {
   constructor (source, message) {
-    super(message)
+    super()
+    this.message = message
     this.source = source
   }
 }
 
 export class YAMLWarning extends Error {
   constructor (source, message) {
-    super(message)
+    super()
+    this.message = message
     this.source = source
   }
 }
