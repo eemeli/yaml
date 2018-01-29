50cb83967764233aa1d1b7e94e6a112fa5d7094f
Eemeli Aro <eemeli@gmail.com>
2018-01-29 13:56:42 +0200

    Add flow collections

--- src/tags/map.js
+++ src/tags/map.js
@@ -1,7 +1,7 @@
 import { Type } from 'raw-yaml'
 import { YAMLSyntaxError } from '../errors'
 
-export function resolveMapPairs (doc, map) {
+export function resolveBlockMapPairs (doc, map) {
   const pairs = []
   let key = undefined
   for (let i = 0; i < map.items.length; ++i) {
@@ -27,6 +27,59 @@ export function resolveMapPairs (doc, map) {
   return pairs
 }
 
+export function resolveFlowMapPairs (doc, map) {
+  const pairs = []
+  let key = undefined
+  let explicitKey = false
+  let next = '{'
+  for (let i = 0; i < map.items.length; ++i) {
+    const item = map.items[i]
+    if (typeof item === 'string') {
+      if (item === '?' && key === undefined && !explicitKey) {
+        // TODO: create errors for non-extended multiline plain keys
+        explicitKey = true
+        next = ':'
+        continue
+      }
+      if (item === ':') {
+        if (key === undefined) key = null
+        if (next === ':') {
+          next = ','
+          continue
+        }
+      } else {
+        if (explicitKey) {
+          if (key === undefined) key = null
+          explicitKey = false
+        }
+        if (key !== undefined) {
+          pairs.push({ key })
+          key === undefined
+        }
+      }
+      if (item === '}') {
+        if (i === node.items.length - 1) continue
+      } else if (item === next) {
+        next = ':'
+        continue
+      }
+      doc.errors.push(new YAMLSyntaxError(node, `Flow map contains an unexpected ${item}`))
+    } else if (item.type === Type.COMMENT) {
+      pairs.push({ comment: item.comment })
+    } else if (key === undefined) {
+      if (next === ',') doc.errors.push(new YAMLSyntaxError(item, 'Separator , missing in flow map'))
+      key = doc.resolveNode(item)
+    } else {
+      if (next !== ',') doc.errors.push(new YAMLSyntaxError(item, 'Indicator : missing in flow map entry'))
+      pairs.push({ key, value: doc.resolveNode(item) })
+      key = undefined
+    }
+  }
+  if (key !== undefined) pairs.push({ key })
+  // TODO: include map & item comments
+  return pairs
+}
+
 export default function resolveMap (doc, node) {
   const comments = []
   class YAMLMap {
@@ -34,7 +87,11 @@ export default function resolveMap (doc, node) {
     toString () { return JSON.stringify(this) }
   }
   const map = node.resolved = new YAMLMap()
-  const pairs = resolveMapPairs(doc, node)
+  const pairs = node.type === Type.FLOW_MAP ? (
+    resolveFlowMapPairs(doc, node)
+  ) : (
+    resolveBlockMapPairs(doc, node)
+  )
   for (let i = 0; i < pairs.length; ++i) {
     const { comment, key, value = null } = pairs[i]
     if (comment) {
--- src/tags/seq.js
+++ src/tags/seq.js
@@ -1,7 +1,7 @@
 import { Type } from 'raw-yaml'
 import { YAMLSyntaxError } from '../errors'
 
-export default function resolveSeq (doc, node) {
+function resolveBlockSeq (doc, node) {
   const seq = node.resolved = []
   const comments = []
   seq.comments = () => comments
@@ -22,3 +22,68 @@ export default function resolveSeq (doc, node) {
   // TODO: include seq & item comments
   return seq
 }
+
+class Pair {
+  constructor (key, value = null) {
+    this.key = key
+    this.value = value
+  }
+}
+
+function resolveFlowSeq (doc, node) {
+  const seq = node.resolved = []
+  const comments = []
+  seq.comments = () => comments
+  seq.toString = () => JSON.stringify(seq)
+  let explicitKey = false
+  let key = undefined
+  let next = '['
+  for (let i = 0; i < node.items.length; ++i) {
+    const item = node.items[i]
+    if (typeof item === 'string') {
+      if (item !== ':' && (explicitKey || key !== undefined)) {
+        if (explicitKey && key === undefined) key = null
+        seq.push(new Pair(key))
+        explicitKey = false
+        key = undefined
+      }
+      if (item === next) {
+        next = null
+      } else if (!next && item === '?') {
+        explicitKey = true
+      } else if (next !== '[' && item === ':' && key === undefined) {
+        key = next === ',' ? seq.pop() : null
+        if (key instanceof Pair) doc.errors.push(new YAMLSyntaxError(item,
+          'Chaining flow sequence pairs is invalid (e.g. [ a : b : c ])'
+        ))
+        explicitKey = false // TODO: add error for non-explicit multiline plain key
+      } else if (next === '[' || item !== ']' || i < node.items.length - 1) {
+        doc.errors.push(new YAMLSyntaxError(node, `Flow sequence contains an unexpected ${item}`))
+      }
+    } else if (item.type === Type.COMMENT) {
+      comments.push({ before: seq.length, comment: item.comment })
+    } else {
+      if (next) doc.errors.push(new YAMLSyntaxError(item, `Expected a ${next} here in flow sequence`))
+      const value = doc.resolveNode(item)
+      if (key === undefined) {
+        seq.push(value)
+      } else {
+        seq.push(new Pair(key, value))
+        key = undefined
+      }
+      next = ','
+    }
+  }
+  if (node.items[node.items.length - 1] !== ']') doc.errors.push(new YAMLSyntaxError(node,
+    'Expected flow sequence to end with ]'
+  ))
+  if (key !== undefined) seq.push(new Pair(key))
+  // TODO: include seq & item comments
+  return seq
+}
+
+export default (doc, node) => node.type === Type.FLOW_SEQ ? (
+  resolveFlowSeq(doc, node)
+) : (
+  resolveBlockSeq(doc, node)
+)
