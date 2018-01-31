1b0609a5b5777776ccde5b114b222660624717b3
Eemeli Aro <eemeli@gmail.com>
2018-02-01 01:21:11 +0200

    Add runner for yaml-test-suite, leave it out of default test run

---
+++ __tests__/yaml-test-suite.js
@@ -0,0 +1,34 @@
+import fs from 'fs'
+import path from 'path'
+
+import resolve from '../src/index'
+
+const testDirs = fs.readdirSync(path.resolve(__dirname, 'yaml-test-suite'))
+  .filter(dir => ['.git', 'meta', 'name', 'tags'].indexOf(dir) === -1)
+
+testDirs.forEach(dir => {
+  const root = path.resolve(__dirname, 'yaml-test-suite', dir)
+  const name = fs.readFileSync(path.resolve(root, '==='), 'utf8')
+  const yaml = fs.readFileSync(path.resolve(root, 'in.yaml'), 'utf8')
+  let json, error
+  try { json = fs.readFileSync(path.resolve(root, 'in.json'), 'utf8') } catch (e) {}
+  try { fs.readFileSync(path.resolve(root, 'error'), 'utf8'); error = true } catch (e) {}
+  if (!error && !json) return
+  test(`${dir}: ${name}`, () => {
+    const stream = resolve(yaml)
+    if (json) {
+      const received = stream[0].toJSON()
+      const expected = JSON.parse(json)
+      if (!received || typeof received !== 'object') {
+        expect(received).toBe(expected)
+      } else {
+        expect(received).toMatchObject(expected)
+      }
+    }
+    if (error) {
+      expect(stream[0].errors).not.toHaveLength(0)
+    } else {
+      expect(stream[0].errors).toHaveLength(0)
+    }
+  })
+})
