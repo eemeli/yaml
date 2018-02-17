9e50c9034377bcdec5b25b31587238f7ab5a8afc
Eemeli Aro <eemeli@gmail.com>
2018-02-17 07:12:28 +0100

    Start testing stringifiers with yaml-test-suite

--- __tests__/yaml-test-suite.js
+++ __tests__/yaml-test-suite.js
@@ -7,6 +7,24 @@ import resolve from '../src/index'
 const testDirs = fs.readdirSync(path.resolve(__dirname, 'yaml-test-suite'))
   .filter(dir => ['.git', 'meta', 'name', 'tags'].indexOf(dir) === -1)
 
+const matchJson = (stream, json) => {
+  if (!json) return
+  const received = stream[0] ? stream[0].toJSON() : null
+  const expected = JSON.parse(json)
+  if (!received || typeof received !== 'object') {
+    expect(received).toBe(expected)
+  } else {
+    expect(received).toMatchObject(expected)
+  }
+}
+
+const skipStringify = [
+  '6FWR', // Block Scalar Keep
+  '6H3V', // Backslashes in singlequotes
+  'LP6E', // Whitespace After Scalars in Flow
+  'Q5MG'  // Tab at beginning of line followed by a flow mapping
+]
+
 testDirs.forEach(dir => {
   const root = path.resolve(__dirname, 'yaml-test-suite', dir)
   const name = fs.readFileSync(path.resolve(root, '==='), 'utf8')
@@ -17,15 +35,7 @@ testDirs.forEach(dir => {
   if (!error && !json) return
   test(`${dir}: ${name}`, () => {
     const stream = resolve(yaml)
-    if (json) {
-      const received = stream[0] ? stream[0].toJSON() : null
-      const expected = JSON.parse(json)
-      if (!received || typeof received !== 'object') {
-        expect(received).toBe(expected)
-      } else {
-        expect(received).toMatchObject(expected)
-      }
-    }
+    matchJson(stream, json)
     if (error) {
       //expect(stream[0].errors).not.toHaveLength(0)
     } else {
@@ -33,6 +43,16 @@ testDirs.forEach(dir => {
         .map(doc => doc.errors.filter(err => !(err instanceof YAMLWarning)))
         .filter(docErrors => docErrors.length > 0)
       expect(errors).toHaveLength(0)
+      if (skipStringify.includes(dir)) return
+      const src2 = stream.map(doc => String(doc)).join('\n---\n')
+      const stream2 = resolve(src2)
+      trace: name,
+        '\nIN\n' + yaml,
+        '\nJSON\n' + JSON.stringify(stream[0], null, '  '),
+        '\n\nOUT\n' + src2,
+        '\nOUT-JSON\n' + JSON.stringify(src2),
+        '\nRE-JSON\n' + JSON.stringify(stream2[0], null, '  ')
+      matchJson(stream2, json)
     }
   })
 })
