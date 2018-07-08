ffc104612cc11e252f16e7f559451c3a2af48123
Eemeli Aro <eemeli@gmail.com>
2018-07-09 02:30:33 +0300

    Add non-error event tests to yaml-test-suite

--- __tests__/yaml-test-suite.js
+++ __tests__/yaml-test-suite.js
@@ -3,6 +3,7 @@ import path from 'path'
 
 import YAML from '../src/index'
 import { strOptions } from '../src/schema/_string'
+import testEvents from '../src/test-events'
 
 const testDirs = fs
   .readdirSync(path.resolve(__dirname, 'yaml-test-suite'))
@@ -41,30 +42,39 @@ afterAll(() => {
 
 testDirs.forEach(dir => {
   const root = path.resolve(__dirname, 'yaml-test-suite', dir)
-  const name = fs.readFileSync(path.resolve(root, '==='), 'utf8')
+  const name = fs.readFileSync(path.resolve(root, '==='), 'utf8').trim()
   const yaml = fs.readFileSync(path.resolve(root, 'in.yaml'), 'utf8')
-  let json, error, outYaml
-  try {
-    json = fs.readFileSync(path.resolve(root, 'in.json'), 'utf8')
-  } catch (e) {}
+  let error, events, json, outYaml
   try {
     fs.readFileSync(path.resolve(root, 'error'), 'utf8')
     error = true
   } catch (e) {}
   try {
+    // Too much variance in event stream length for error cases
+    events = !error && fs.readFileSync(path.resolve(root, 'test.event'), 'utf8')
+  } catch (e) {}
+  try {
+    json = fs.readFileSync(path.resolve(root, 'in.json'), 'utf8')
+  } catch (e) {}
+  try {
     outYaml = fs.readFileSync(path.resolve(root, 'out.yaml'), 'utf8')
   } catch (e) {}
-  if (!error && !json && !outYaml) return
-  test(`${dir}: ${name}`, () => {
+
+  describe(`${dir}: ${name}`, () => {
     const docs = YAML.parseAllDocuments(yaml)
-    matchJson(docs, json)
-    const errors = docs
-      .map(doc => doc.errors)
-      .filter(docErrors => docErrors.length > 0)
-    if (error) {
-      expect(errors).not.toHaveLength(0)
-    } else {
-      expect(errors).toHaveLength(0)
+    if (events) test('test.event', () => expect(testEvents(yaml)).toBe(events))
+    if (json) test('in.json', () => matchJson(docs, json))
+    test('errors', () => {
+      const errors = docs
+        .map(doc => doc.errors)
+        .filter(docErrors => docErrors.length > 0)
+      if (error) {
+        expect(errors).not.toHaveLength(0)
+      } else {
+        expect(errors).toHaveLength(0)
+      }
+    })
+    if (!error) {
       const src2 =
         docs.map(doc => String(doc).replace(/\n$/, '')).join('\n...\n') + '\n'
       const docs2 = YAML.parseAllDocuments(src2)
@@ -74,12 +84,14 @@ testDirs.forEach(dir => {
         '\n\nOUT\n' + src2,
         '\nOUT-JSON\n' + JSON.stringify(src2),
         '\nRE-JSON\n' + JSON.stringify(docs2[0], null, '  ')
-      matchJson(docs2, json)
+      if (json) test('stringfy+re-parse', () => matchJson(docs2, json))
       if (outYaml) {
-        const expDocs = YAML.parseAllDocuments(outYaml)
-        const resJson = docs.map(doc => doc.toJSON())
-        const expJson = expDocs.map(doc => doc.toJSON())
-        expect(resJson).toMatchObject(expJson)
+        test('out.yaml', () => {
+          const expDocs = YAML.parseAllDocuments(outYaml)
+          const resJson = docs.map(doc => doc.toJSON())
+          const expJson = expDocs.map(doc => doc.toJSON())
+          expect(resJson).toMatchObject(expJson)
+        })
       }
     }
   })
