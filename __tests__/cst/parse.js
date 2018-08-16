dafc1ed67bc6147118f1d73769697f20f4385c49
Eemeli Aro <eemeli@gmail.com>
2018-08-15 18:43:29 -0700

    cst/parse: Add tests

---
+++ __tests__/cst/parse.js
@@ -0,0 +1,203 @@
+import parse from '../../src/cst/parse'
+
+test('return value', () => {
+  const src = '- foo\n- bar\n'
+  const cst = parse(src)
+  expect(cst).toHaveLength(1)
+  expect(cst[0]).toMatchObject({
+    contents: [
+      {
+        error: null,
+        items: [
+          {
+            error: null,
+            node: {
+              error: null,
+              props: [],
+              range: { end: 6, start: 2 },
+              type: 'PLAIN',
+              value: null,
+              valueRange: { end: 5, start: 2 }
+            },
+            props: [],
+            range: { end: 6, start: 0 },
+            type: 'SEQ_ITEM',
+            value: null,
+            valueRange: { end: 5, start: 0 }
+          },
+          {
+            error: null,
+            node: {
+              error: null,
+              props: [],
+              range: { end: 12, start: 8 },
+              type: 'PLAIN',
+              value: null,
+              valueRange: { end: 11, start: 8 }
+            },
+            props: [],
+            range: { end: 12, start: 6 },
+            type: 'SEQ_ITEM',
+            value: null,
+            valueRange: { end: 11, start: 6 }
+          }
+        ],
+        props: [],
+        range: { end: 12, start: 0 },
+        type: 'SEQ',
+        value: null,
+        valueRange: { end: 11, start: 0 }
+      }
+    ],
+    directives: [],
+    error: null,
+    props: [],
+    range: null,
+    type: 'DOCUMENT',
+    value: null,
+    valueRange: { end: 11, start: 0 }
+  })
+})
+
+describe('toString()', () => {
+  test('plain document', () => {
+    const src = '- foo\n- bar\n'
+    const cst = parse(src)
+    expect(String(cst)).toBe(src)
+  })
+
+  test('stream of two documents', () => {
+    const src = 'foo\n...\nbar\n'
+    const cst = parse(src)
+    expect(cst).toHaveLength(2)
+    expect(String(cst)).toBe(src)
+  })
+
+  test('document with CRLF line separators', () => {
+    const src = '- foo\r\n- bar\r\n'
+    const cst = parse(src)
+    expect(cst.toString()).toBe('- foo\n- bar\n')
+  })
+})
+
+describe('setOrigRanges()', () => {
+  test('return false for no CRLF', () => {
+    const src = '- foo\n- bar\n'
+    const cst = parse(src)
+    expect(cst.setOrigRanges()).toBe(false)
+    expect(cst[0].valueRange).toMatchObject({ start: 0, end: 11 })
+    expect(cst[0].valueRange.origStart).toBeUndefined()
+    expect(cst[0].valueRange.origEnd).toBeUndefined()
+  })
+
+  test('single document', () => {
+    const src = '- foo\r\n- bar\r\n'
+    const cst = parse(src)
+    expect(cst.setOrigRanges()).toBe(true)
+    expect(cst).toHaveLength(1)
+    const { range, valueRange } = cst[0].contents[0].items[1].node
+    expect(src.slice(range.origStart, range.origEnd)).toBe('bar\r\n')
+    expect(src.slice(valueRange.origStart, valueRange.origEnd)).toBe('bar')
+    expect(cst[0]).toMatchObject({
+      contents: [
+        {
+          error: null,
+          items: [
+            {
+              error: null,
+              node: {
+                error: null,
+                props: [],
+                range: { end: 6, origEnd: 7, origStart: 2, start: 2 },
+                type: 'PLAIN',
+                value: null,
+                valueRange: { end: 5, origEnd: 5, origStart: 2, start: 2 }
+              },
+              props: [],
+              range: { end: 6, origEnd: 7, origStart: 0, start: 0 },
+              type: 'SEQ_ITEM',
+              value: null,
+              valueRange: { end: 5, origEnd: 5, origStart: 0, start: 0 }
+            },
+            {
+              error: null,
+              node: {
+                error: null,
+                props: [],
+                range: { end: 12, origEnd: 14, origStart: 9, start: 8 },
+                type: 'PLAIN',
+                value: null,
+                valueRange: { end: 11, origEnd: 12, origStart: 9, start: 8 }
+              },
+              props: [],
+              range: { end: 12, origEnd: 14, origStart: 7, start: 6 },
+              type: 'SEQ_ITEM',
+              value: null,
+              valueRange: { end: 11, origEnd: 12, origStart: 7, start: 6 }
+            }
+          ],
+          props: [],
+          range: { end: 12, origEnd: 14, origStart: 0, start: 0 },
+          type: 'SEQ',
+          value: null,
+          valueRange: { end: 11, origEnd: 12, origStart: 0, start: 0 }
+        }
+      ],
+      directives: [],
+      error: null,
+      props: [],
+      range: null,
+      type: 'DOCUMENT',
+      value: null,
+      valueRange: { end: 11, origEnd: 12, origStart: 0, start: 0 }
+    })
+  })
+
+  test('stream of two documents', () => {
+    const src = 'foo\r\n...\r\nbar\r\n'
+    const cst = parse(src)
+    expect(cst.setOrigRanges()).toBe(true)
+    expect(cst).toHaveLength(2)
+    const { range, valueRange } = cst[1].contents[0]
+    expect(src.slice(range.origStart, range.origEnd)).toBe('bar\r\n')
+    expect(src.slice(valueRange.origStart, valueRange.origEnd)).toBe('bar')
+    expect(cst[0]).toMatchObject({
+      contents: [
+        {
+          error: null,
+          props: [],
+          range: { end: 4, origEnd: 5, origStart: 0, start: 0 },
+          type: 'PLAIN',
+          value: null,
+          valueRange: { end: 3, origEnd: 3, origStart: 0, start: 0 }
+        }
+      ],
+      directives: [],
+      error: null,
+      props: [],
+      range: null,
+      type: 'DOCUMENT',
+      value: null,
+      valueRange: { end: 3, origEnd: 3, origStart: 0, start: 0 }
+    })
+    expect(cst[1]).toMatchObject({
+      contents: [
+        {
+          error: null,
+          props: [],
+          range: { end: 12, origEnd: 15, origStart: 10, start: 8 },
+          type: 'PLAIN',
+          value: null,
+          valueRange: { end: 11, origEnd: 13, origStart: 10, start: 8 }
+        }
+      ],
+      directives: [],
+      error: null,
+      props: [],
+      range: null,
+      type: 'DOCUMENT',
+      value: null,
+      valueRange: { end: 11, origEnd: 13, origStart: 10, start: 8 }
+    })
+  })
+})
