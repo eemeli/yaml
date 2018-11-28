049c62e48247e6e0fe63ef45b8a9e8c9663b962b
Eemeli Aro <eemeli@gmail.com>
2018-11-28 14:16:23 +0200

    Support creating Map nodes from JS Map objects (Fixes #49)

--- __tests__/createNode.js
+++ __tests__/createNode.js
@@ -1,6 +1,6 @@
 import createNode from '../src/createNode'
 import YAML from '../src/index'
-import Map from '../src/schema/Map'
+import YAMLMap from '../src/schema/Map'
 import Pair from '../src/schema/Pair'
 import Scalar from '../src/schema/Scalar'
 import Seq from '../src/schema/Seq'
@@ -107,12 +107,12 @@ describe('arrays', () => {
 describe('objects', () => {
   test('createNode({})', () => {
     const s = createNode({})
-    expect(s).toBeInstanceOf(Map)
+    expect(s).toBeInstanceOf(YAMLMap)
     expect(s.items).toHaveLength(0)
   })
   test('createNode({ x: true }, false)', () => {
     const s = createNode({ x: true }, false)
-    expect(s).toBeInstanceOf(Map)
+    expect(s).toBeInstanceOf(YAMLMap)
     expect(s.items).toHaveLength(1)
     expect(s.items[0]).toBeInstanceOf(Pair)
     expect(s.items[0]).toMatchObject({ key: 'x', value: true })
@@ -121,7 +121,7 @@ describe('objects', () => {
     const object = { x: 3, y: [4], z: { w: 'five', v: 6 } }
     test('createNode(value, false)', () => {
       const s = createNode(object, false)
-      expect(s).toBeInstanceOf(Map)
+      expect(s).toBeInstanceOf(YAMLMap)
       expect(s.items).toHaveLength(3)
       expect(s.items).toMatchObject([
         { key: 'x', value: 3 },
@@ -136,7 +136,7 @@ describe('objects', () => {
     })
     test('createNode(value, true)', () => {
       const s = createNode(object, true)
-      expect(s).toBeInstanceOf(Map)
+      expect(s).toBeInstanceOf(YAMLMap)
       expect(s.items).toHaveLength(3)
       expect(s.items).toMatchObject([
         { key: { value: 'x' }, value: { value: 3 } },
@@ -181,9 +181,9 @@ describe('Set', () => {
     expect(s).toBeInstanceOf(Seq)
     expect(s.items).toMatchObject([true])
   })
-  describe('new Set([3, new Set(["four", 5])])', () => {
+  describe("Set { 3, Set { 'four', 5 } }", () => {
     const set = new Set([3, new Set(['four', 5])])
-    test('createNode(value, false)', () => {
+    test('createNode(set, false)', () => {
       const s = createNode(set, false)
       expect(s).toBeInstanceOf(Seq)
       expect(s.items).toHaveLength(2)
@@ -191,7 +191,7 @@ describe('Set', () => {
       expect(s.items[1]).toBeInstanceOf(Seq)
       expect(s.items[1].items).toMatchObject(['four', 5])
     })
-    test('createNode(value, true)', () => {
+    test('createNode(set, true)', () => {
       const s = createNode(set, true)
       expect(s).toBeInstanceOf(Seq)
       expect(s.items).toHaveLength(2)
@@ -209,6 +209,76 @@ describe('Set', () => {
       doc.contents = createNode(set, false)
       expect(String(doc)).toBe(res)
       doc.contents = createNode(set, true)
+      expect(String(doc)).toBe(res)
+    })
+  })
+})
+
+describe('Map', () => {
+  test('createNode(new Map)', () => {
+    const s = createNode(new Map())
+    expect(s).toBeInstanceOf(YAMLMap)
+    expect(s.items).toHaveLength(0)
+  })
+  test('createNode(new Map([["x", true]]), false)', () => {
+    const s = createNode(new Map([['x', true]]), false)
+    expect(s).toBeInstanceOf(YAMLMap)
+    expect(s.items).toHaveLength(1)
+    expect(s.items[0]).toBeInstanceOf(Pair)
+    expect(s.items[0]).toMatchObject({ key: 'x', value: true })
+  })
+  describe("Map { 'x' => 3, 'y' => Set { 4 }, Map { 'w' => 'five', 'v' => 6 } => 'z' }", () => {
+    const map = new Map([
+      ['x', 3],
+      ['y', new Set([4])],
+      [new Map([['w', 'five'], ['v', 6]]), 'z']
+    ])
+    test('createNode(map, false)', () => {
+      const s = createNode(map, false)
+      expect(s).toBeInstanceOf(YAMLMap)
+      expect(s.items).toHaveLength(3)
+      expect(s.items).toMatchObject([
+        { key: 'x', value: 3 },
+        { key: 'y', value: { items: [4] } },
+        {
+          key: {
+            items: [{ key: 'w', value: 'five' }, { key: 'v', value: 6 }]
+          },
+          value: 'z'
+        }
+      ])
+    })
+    test('createNode(map, true)', () => {
+      const s = createNode(map, true)
+      expect(s).toBeInstanceOf(YAMLMap)
+      expect(s.items).toHaveLength(3)
+      expect(s.items).toMatchObject([
+        { key: { value: 'x' }, value: { value: 3 } },
+        { key: { value: 'y' }, value: { items: [{ value: 4 }] } },
+        {
+          key: {
+            items: [
+              { key: { value: 'w' }, value: { value: 'five' } },
+              { key: { value: 'v' }, value: { value: 6 } }
+            ]
+          },
+          value: { value: 'z' }
+        }
+      ])
+    })
+    test('set doc contents', () => {
+      const res = `x: 3
+y:
+  - 4
+? w: five
+  v: 6
+: z\n`
+      const doc = new YAML.Document()
+      doc.contents = map
+      expect(String(doc)).toBe(res)
+      doc.contents = createNode(map, false)
+      expect(String(doc)).toBe(res)
+      doc.contents = createNode(map, true)
       expect(String(doc)).toBe(res)
     })
   })
