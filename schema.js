b9f1df97ed5f4095964dedfec62f26a29480f9c6
Eemeli Aro <eemeli@gmail.com>
2018-12-12 00:20:27 +0200

    Add public export for schema

---
+++ schema.js
@@ -0,0 +1,3 @@
+module.exports = require('./dist/schema')
+module.exports.nullOptions = require('./dist/schema/core').nullOptions
+module.exports.strOptions = require('./dist/schema/_string').strOptions
