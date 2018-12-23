8ef5c0d593c4ab9ce5339fd41eedb506bcaf5765
Nicolas DUBIEN <dubzzz@users.noreply.github.com>
2018-12-23 16:29:10 +0100

    Add property based tests using fast-check (#62)
    This commit is a follow up of the issues opened by @rix0rrr recently:
    - #56
    - #57
    - #59

---
+++ __tests__/properties.js
@@ -0,0 +1,35 @@
+import YAML from '../src/index'
+import * as fc from 'fast-check'
+
+describe('properties', () => {
+  test('parse stringified object', () => {
+    const key = fc.fullUnicodeString()
+    const values = [
+      key,
+      fc.lorem(1000, false), // words
+      fc.lorem(100, true), // sentences
+      fc.boolean(),
+      fc.integer(),
+      fc.double(),
+      fc.constantFrom(null, Number.NEGATIVE_INFINITY, Number.POSITIVE_INFINITY)
+    ]
+    const yamlArbitrary = fc.anything({ key: key, values: values })
+    const optionsArbitrary = fc.record(
+      {
+        keepBlobsInJSON: fc.boolean(),
+        keepCstNodes: fc.boolean(),
+        keepNodeTypes: fc.boolean(),
+        mapAsMap: fc.constant(false),
+        merge: fc.boolean(),
+        schema: fc.constantFrom('core', 'yaml-1.1') // ignore 'failsafe', 'json'
+      },
+      { withDeletedKeys: true }
+    )
+
+    fc.assert(
+      fc.property(yamlArbitrary, optionsArbitrary, (obj, opts) => {
+        expect(YAML.parse(YAML.stringify(obj, opts), opts)).toStrictEqual(obj)
+      })
+    )
+  })
+})
