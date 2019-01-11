e3121bfc1f0f4d919cb998354f0e2ec119d4004d
Eemeli Aro <eemeli@gmail.com>
2019-01-11 18:40:39 +0000

    Add separate build for browsers (note: will require @babel/runtime)

--- .babelrc.js
+++ .babelrc.js
@@ -1,8 +1,14 @@
-module.exports = {
-  presets: [['@babel/env', { targets: { node: '6.5' } }]],
-  plugins: [
-    '@babel/plugin-proposal-class-properties',
-    ['babel-plugin-trace', { strip: true }],
-    ['babel-plugin-add-module-exports', { addDefaultProperty: true }]
-  ]
-}
+const isBrowser = process.env.BABEL_ENV === 'browser'
+
+const envOptions = { useBuiltIns: false }
+if (!isBrowser) envOptions.targets = { node: '6.5' }
+const presets = [['@babel/env', envOptions]]
+
+const plugins = [
+  '@babel/plugin-proposal-class-properties',
+  ['babel-plugin-trace', { strip: true }],
+  ['babel-plugin-add-module-exports', { addDefaultProperty: true }]
+]
+if (isBrowser) plugins.push('@babel/plugin-transform-runtime')
+
+module.exports = { presets, plugins }
