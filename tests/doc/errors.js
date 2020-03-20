1c79fca7994880aea01dd934523bdc7f1b673524
Eemeli Aro <eemeli@gmail.com>
2020-03-20 23:35:12 +0200

    Add Jest workaround to file deprecation tests

--- tests/doc/errors.js
+++ tests/doc/errors.js
@@ -295,9 +295,11 @@ describe('deprecations', () => {
   const root = path.resolve(__dirname, '../..')
   const fileTest = fs.existsSync(path.resolve(root, 'dist')) ? test : test.skip
   for (const file of files)
-    fileTest(`file: ${file}`, async () => {
+    fileTest(`file: ${file}`, () => {
       const fp = path.resolve(root, file)
-      await import(fp)
+      // await import() may fail with ERR_VM_DYNAMIC_IMPORT_CALLBACK_MISSING due
+      // to https://github.com/facebook/jest/issues/9430
+      require(fp)
       expect(mock).toHaveBeenCalledTimes(1)
     })
 })
