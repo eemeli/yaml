dc428a437c9428ed626c1adcaec9a3ee28ceac06
Eemeli Aro <eemeli@gmail.com>
2020-03-20 23:21:02 +0200

    Add custom ES module exports for browsers

---
+++ browser/util.js
@@ -0,0 +1,17 @@
+export { findPair } from './dist/schema/Map'
+export { parseMap } from './dist/schema/parseMap'
+export { parseSeq } from './dist/schema/parseSeq'
+
+export {
+  stringifyNumber,
+  stringifyString,
+  toJSON,
+  Type
+} from './dist/stringify'
+
+export {
+  YAMLReferenceError,
+  YAMLSemanticError,
+  YAMLSyntaxError,
+  YAMLWarning
+} from './dist/errors'
