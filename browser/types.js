aad3ee5fa44fc659f139b15716be7e9d507dff71
Eemeli Aro <eemeli@gmail.com>
2020-03-20 23:21:02 +0200

    Add custom ES module exports for browsers

---
+++ browser/types.js
@@ -0,0 +1,12 @@
+export {
+  binaryOptions,
+  boolOptions,
+  nullOptions,
+  strOptions
+} from './dist/tags/options'
+
+export { Schema } from './dist/schema'
+export { YAMLMap } from './dist/schema/Map'
+export { YAMLSeq } from './dist/schema/Seq'
+export { Pair } from './dist/schema/Pair'
+export { Scalar } from './dist/schema/Scalar'
