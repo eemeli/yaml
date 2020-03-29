8db05df151c081ba1d4ce4d5e539de66606dfe40
Eemeli Aro <eemeli@gmail.com>
2020-03-29 12:02:33 +0300

    Merge {map,pair,scalar,seq}.d.ts into types.d.ts

---
+++ types.d.ts
@@ -0,0 +1,6 @@
+import * as YAML from 'yaml'
+
+export const YAMLMap: YAML.ast.MapConstructor
+export const YAMLSeq: YAML.ast.SeqConstructor
+export const Pair: YAML.ast.PairConstructor
+export const Scalar: YAML.ast.ScalarConstructor
