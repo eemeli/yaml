aa72891708065109ab937c4c0bf7d6f8439b7a96
Eemeli Aro <eemeli@gmail.com>
2020-05-16 13:43:40 +0300

    Drop warnFileDeprecation() and warnOptionDeprecation(), refactor & test warn()

--- src/legacy-exports.js
+++ src/legacy-exports.js
@@ -3,5 +3,3 @@ export { omap } from './tags/yaml-1.1/omap.js'
 export { pairs } from './tags/yaml-1.1/pairs.js'
 export { set } from './tags/yaml-1.1/set.js'
 export { floatTime, intTime, timestamp } from './tags/yaml-1.1/timestamp.js'
-
-export { warnFileDeprecation } from './warnings'
--- src/warnings.js
+++ src/warnings.js
@@ -1,51 +1,20 @@
-/* global console, process, YAML_SILENCE_DEPRECATION_WARNINGS, YAML_SILENCE_WARNINGS */
-
-function shouldWarn(deprecation) {
-  const env = (typeof process !== 'undefined' && process.env) || {}
-
-  if (deprecation) {
-    if (typeof YAML_SILENCE_DEPRECATION_WARNINGS !== 'undefined')
-      return !YAML_SILENCE_DEPRECATION_WARNINGS
-    return !env.YAML_SILENCE_DEPRECATION_WARNINGS
-  }
-
-  if (typeof YAML_SILENCE_WARNINGS !== 'undefined')
-    return !YAML_SILENCE_WARNINGS
-  return !env.YAML_SILENCE_WARNINGS
-}
+/* global console, process, YAML_SILENCE_WARNINGS */
 
 export function warn(warning, type) {
-  if (shouldWarn(false)) {
-    const emit = typeof process !== 'undefined' && process.emitWarning
+  if (typeof YAML_SILENCE_WARNINGS !== 'undefined' && YAML_SILENCE_WARNINGS)
+    return
+
+  if (typeof process !== 'undefined') {
+    if (process.env.YAML_SILENCE_WARNINGS) return
+
     // This will throw in Jest if `warning` is an Error instance due to
     // https://github.com/facebook/jest/issues/2549
-    if (emit) emit(warning, type)
-    else {
-      // eslint-disable-next-line no-console
-      console.warn(type ? `${type}: ${warning}` : warning)
+    if (process.emitWarning) {
+      process.emitWarning(warning, type)
+      return
     }
   }
-}
 
-export function warnFileDeprecation(filename) {
-  if (shouldWarn(true)) {
-    const path = filename
-      .replace(/.*yaml[/\\]/i, '')
-      .replace(/\.js$/, '')
-      .replace(/\\/g, '/')
-    warn(
-      `The endpoint 'yaml/${path}' will be removed in a future release.`,
-      'DeprecationWarning'
-    )
-  }
-}
-
-const warned = {}
-export function warnOptionDeprecation(name, alternative) {
-  if (!warned[name] && shouldWarn(true)) {
-    warned[name] = true
-    let msg = `The option '${name}' will be removed in a future release`
-    msg += alternative ? `, use '${alternative}' instead.` : '.'
-    warn(msg, 'DeprecationWarning')
-  }
+  // eslint-disable-next-line no-console
+  console.warn(type ? `${type}: ${warning}` : warning)
 }
