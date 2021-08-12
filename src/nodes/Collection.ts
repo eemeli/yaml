aa724ec3d97f23937bc7a329eb2ef58c5d479fb0
Eemeli Aro <eemeli@gmail.com>
2021-08-12 10:52:43 +0300

    fix: Allow for collection setIn() to handle objects, including duplicates

--- src/nodes/Collection.ts
+++ src/nodes/Collection.ts
@@ -15,21 +15,14 @@ export function collectionFromPath(
       a[k] = v
       v = a
     } else {
-      const o = {}
-      Object.defineProperty(o, typeof k === 'symbol' ? k : String(k), {
-        value: v,
-        writable: true,
-        enumerable: true,
-        configurable: true
-      })
-      v = o
+      v = new Map<unknown, unknown>([[k, v]])
     }
   }
   return createNode(v, undefined, {
-    aliasDuplicateObjects: true,
+    aliasDuplicateObjects: false,
     keepUndefined: false,
-    onAnchor() {
-      throw new Error('Repeated objects are not supported here')
+    onAnchor: () => {
+      throw new Error('This should not happen, please report a bug.')
     },
     schema,
     sourceObjects: new Map()
