a9733ff1712cfceff69d237a649154c84acc194a
Eemeli Aro <eemeli@gmail.com>
2021-11-28 14:30:31 +0200

    test: Update to latest yaml-test-suite data, skipping 5T43 for now

--- tests/yaml-test-suite.ts
+++ tests/yaml-test-suite.ts
@@ -7,6 +7,8 @@ import { CST, Document, Lexer, parseAllDocuments, Parser } from 'yaml'
 import { testEvents } from 'yaml/test-events'
 
 const skip: Record<string, boolean | string[]> = {
+  '5T43': true, // FIXME
+  '9MMA': ['errors'], // allow stream with directive & no docs
   B63P: ['errors'], // allow ... after directives
   SF5V: ['errors'] // allow duplicate %YAML directives
 }
