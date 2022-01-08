7998a957d1b9cecffc0a79f6023c6882e073d6eb
Eemeli Aro <eemeli@gmail.com>
2022-01-08 18:13:56 +0200

    feat!: Remove YAML.defaultOptions (#346)

--- src/options.ts
+++ src/options.ts
@@ -372,22 +372,3 @@ export type ToStringOptions = {
    */
   verifyAliasOrder?: boolean
 }
-
-/**
- * `yaml` defines document-specific options in three places: as an argument of
- * parse, create and stringify calls, in the values of `YAML.defaultOptions`,
- * and in the version-dependent `YAML.Document.defaults` object. Values set in
- * `YAML.defaultOptions` override version-dependent defaults, and argument
- * options override both.
- */
-export const defaultOptions: Required<
-  Omit<ParseOptions, 'lineCounter'> & Omit<DocumentOptions, 'directives'>
-> = {
-  intAsBigInt: false,
-  keepSourceTokens: false,
-  logLevel: 'warn',
-  prettyErrors: true,
-  strict: true,
-  uniqueKeys: true,
-  version: '1.2'
-}
