d1aca82bc15a4c261bdc58561d32189a5d3a45ef
David Thompson <davidethompson@me.com>
2026-03-13 09:25:17 -0400

    Add trailingComma ToString option for multiline flow formatting (#670)
    Disabled by default. When enabled, the last entry in a flow map or flow
    sequence will have a `,` after it if the collection is split across multiple lines.
    
    ---------
    
    Signed-off-by: David Thompson <davthomp@redhat.com>

--- src/stringify/stringify.ts
+++ src/stringify/stringify.ts
@@ -55,6 +55,7 @@ export function createStringifyContext(
       nullStr: 'null',
       simpleKeys: false,
       singleQuote: null,
+      trailingComma: false,
       trueStr: 'true',
       verifyAliasOrder: true
     },
