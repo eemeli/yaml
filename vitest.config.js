6708a3e5c05b9f7dc4fd9c716ff57effc3ce452b
Eemeli Aro <eemeli@gmail.com>
2026-04-18 14:23:56 +0300

    chore: Drop deprecated resolve.alias customResolvers from vitest configs

--- vitest.config.js
+++ vitest.config.js
@@ -2,39 +2,22 @@ import { resolve } from 'node:path'
 import { env } from 'node:process'
 import { defineConfig } from 'vitest/config'
 
-let alias
-if (env.TEST_DIST || env.npm_lifecycle_event === 'test:dist') {
+let getPath
+if (!!env.TEST_DIST || env.npm_lifecycle_event === 'test:dist') {
   console.log('Testing build output from dist/')
-  alias = [
-    {
-      find: /^yaml/,
-      /** @param {string} path */
-      customResolver(path) {
-        const name = path.split('/')[1] ?? 'index'
-        return resolve(import.meta.dirname, 'dist', `${name}.js`)
-      }
-    },
-    {
-      find: '../src/test-events.ts',
-      replacement: resolve(import.meta.dirname, 'dist', 'test-events.js')
-    }
-  ]
+  getPath = name => resolve(import.meta.dirname, 'dist', `${name}.js`)
 } else {
-  alias = [
-    {
-      find: /^yaml/,
-      /** @param {string} path */
-      customResolver(path) {
-        const name = path.split('/')[1] ?? 'index'
-        return resolve(import.meta.dirname, 'src', `${name}.ts`)
-      }
-    }
-  ]
+  getPath = name => resolve(import.meta.dirname, 'src', `${name}.ts`)
 }
 
 export default defineConfig({
   test: {
-    alias,
+    alias: {
+      '../src/test-events.ts': getPath('test-events'),
+      'yaml/cli': getPath('cli'),
+      'yaml/util': getPath('util'),
+      yaml: getPath('index')
+    },
     globals: true,
     setupFiles: ['tests/_setup.ts'],
     include: ['tests/**/*.{js,ts}'],
