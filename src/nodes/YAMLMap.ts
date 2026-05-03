ac9a8caafd2177b2a1baaeec36d1a0c1bfb5a8b8
Remco Haszing <remcohaszing@gmail.com>
2026-05-03 12:23:22 +0200

    Fix lint issues

--- src/nodes/YAMLMap.ts
+++ src/nodes/YAMLMap.ts
@@ -239,7 +239,7 @@ export class YAMLMap<
       ? new Type()
       : ctx?.mapAsMap
         ? new Map()
-        : Object.create(null)
+        : (Object.create(null) as Record<string, unknown>)
     if (this.anchor) ctx.setAnchor(this, map)
     for (const pair of this.values.values()) addPairToJSMap(doc, ctx, map, pair)
     return map
